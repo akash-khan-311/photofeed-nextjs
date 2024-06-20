76ecee1bf33fb7231c53e8674ebcdbd79711d2e0
Akash Khan <akashcc48@gmail.com git config --global user.email akashcc48@gmail.com git config --global user.email akashcc48@gmail.com>
2024-06-20 19:55:58 +0600

    navbar, logo and layouts added

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,11 +1,17 @@
+import Image from "next/image";
+import Link from "next/link";
 
 const Logo = () => {
-    return (
-        <section className=''>
-            <div className=''>
-                Welcome to Logo
-            </div>
-        </section>
-    )
-}
+  return (
+    <Link href="/">
+      <Image
+        class="max-w-[100px] md:max-w-[165px]"
+        width={100}
+        height={165}
+        src="/lws_logo.png"
+        alt="Lws"
+      />
+    </Link>
+  );
+};
 export default Logo;
